fd21cee0835c85e94941b9cf5c67308d8d2574b9
Ehsan <e.rainyman2012@gmail.com>
2021-08-16 12:05:49 +0430

    Update test.py

--- client1/test.py
+++ client1/test.py
@@ -1,8 +1,7 @@
 import redis
 import time
 
-r = redis.Redis(host= 'localhost', port= '6379')
-
+r = redis.Redis(host = 'localhost', port = '6379')
 
 def upper():
     next_value = 0
@@ -10,7 +9,6 @@ def upper():
         error_count = 0
         while True:
             try:
-    
                 pipe.watch('counter')
                 current_value = int(pipe.get('counter')) if pipe.get('counter') else 0
                 next_value = current_value + 1
@@ -18,7 +16,6 @@ def upper():
                 pipe.set('counter', next_value, None)
                 print("value:",  next_value)
                 pipe.execute()
-                
                 break
             except redis.WatchError:
                 # show which counter is used by another user
